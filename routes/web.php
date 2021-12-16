e73ab20e70294f07e15b83ff7d00e2965c79c96e
sony_wang <phenombox@gmail.com>
2021-12-16 23:19:23 +0800

    接到資料了

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UsersController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/users', function () {
+//     return view('users');
+// });
+// Route::get('/users', 'UsersController@index');
+Route::get('/users', 'App\Http\Controllers\UsersController@index');
+
+// Route::resource('users', 'UsersController');
