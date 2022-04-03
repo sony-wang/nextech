24dc4fc3fd640a971a6a4a51f70bb65807d5f0ed
sony <phenombox@gmail.com>
2022-04-03 17:30:39 +0800

    共學班也加入PDF下載

--- app/Admin/Controllers/Class02sController.php
+++ app/Admin/Controllers/Class02sController.php
@@ -149,98 +149,205 @@ class Class02sController extends AdminController
         $show->field('upload2', __('Com_reg_cert'))->unescape()->as(function ($title) {
             return "<a href='{$title}' target='_blank'>下載</a>";
         });
-        $show->field('ques_s', __('Ques_s'))->unescape()->as(function ($title) {
-            $content_ok = [];
+        $show->field('id', __('問券'))->unescape()->as(function ($id) {
+            $content_ok_s = [];
             //題目
             $questions = DB::table('questions')->select('category_id', 'content')->get();
             $ques = json_decode($questions, JSON_UNESCAPED_UNICODE);
             foreach($ques as $key=>$item){
-                array_push($content_ok, $item);
+                array_push($content_ok_s, $item);
             }
+
+            $class01Data = DB::table('class02s')->select('company','ques_s','ques_m')->where('id',$id)->get();
             //答案
-            $newTitle = json_decode("{$title}", true);
+            $newclass01Data = json_decode("{$class01Data}", true);
+            // Log::info('112233');
+            // Log::info($newclass01Data[0]['company']);
             $tmpKey = 0;
-            foreach($newTitle as $key=>$ansNO){
-                $content_ok[$tmpKey]['ansNO'] = $ansNO;
-                // array_push($content_ok[$tmpKey], $ansNO);
-                $tmpKey++;
-            }
-            //選項
-            $question_categories_s = DB::table('question_categories')->select('options', 'category')->where('choice','S')->get();
-            $ques_cate_s = json_decode($question_categories_s, JSON_UNESCAPED_UNICODE);
-            $quesCount = DB::table('questions')->select(DB::raw('count(*) as total'))->groupBy('category_id')->get();
-            for($i=1;$i<=count($quesCount);$i++){
-                foreach($content_ok as $key=>$item){
-                    if($item['category_id'] == $i){
-                        if($item['ansNO'] == 1){
-                            $content_ok[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[0];
-                        }
-                        if($item['ansNO'] == 2){
-                            $content_ok[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[1];
-                        }
-                        if($item['ansNO'] == 3){
-                            $content_ok[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[2];
-                        }
-                        if($item['ansNO'] == 4){
-                            $content_ok[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[3];
-                        }
-                        if($item['ansNO'] == 5){
-                            $content_ok[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[4];
+            if(!empty( $newclass01Data[0]['ques_s'] )){
+                foreach(json_decode($newclass01Data[0]['ques_s']) as $key=>$ansNO){
+                    $content_ok_s[$tmpKey]['ansNO'] = $ansNO;
+                    // array_push($content_ok_s[$tmpKey], $ansNO);
+                    $tmpKey++;
+                }
+                //選項
+                $question_categories_s = DB::table('question_categories')->select('options', 'category')->where('choice','S')->get();
+                $ques_cate_s = json_decode($question_categories_s, JSON_UNESCAPED_UNICODE);
+                $quesCount = DB::table('questions')->select(DB::raw('count(*) as total'))->groupBy('category_id')->get();
+                for($i=1;$i<=count($quesCount);$i++){
+                    foreach($content_ok_s as $key=>$item){
+                        if($item['category_id'] == $i){
+                            if($item['ansNO'] == 1){
+                                $content_ok_s[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[0];
+                            }
+                            if($item['ansNO'] == 2){
+                                $content_ok_s[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[1];
+                            }
+                            if($item['ansNO'] == 3){
+                                $content_ok_s[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[2];
+                            }
+                            if($item['ansNO'] == 4){
+                                $content_ok_s[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[3];
+                            }
+                            if($item['ansNO'] == 5){
+                                $content_ok_s[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[4];
+                            }
                         }
                     }
                 }
-            }
-            // Log::info($content_ok);
-            $dom = "<a class='btn btn-primary' href='/getDate?val=$title' target='_blank'>下載</a>";
-            $dom .= "<ul>";
-            foreach($content_ok as $item){
-                $dom .= "<li>[ {$item['ansNO']}.{$item['ansTxt']} ] - {$item['content']}</li>";
-            }
-            $dom .= "<ul>";
-            return "{$dom}";
-        });
-        $show->field('ques_m', __('Ques_m'))->unescape()->as(function ($title) {
-            $content_ok = [];
-            $question_categories_m = DB::table('question_categories')->select('options', 'category','customize')->where('choice','M')->get();
-            $ques_cate_m = json_decode($question_categories_m, JSON_UNESCAPED_UNICODE);
-            
-            // Log::info($ques_cate_m);
-            //答案
-            $newTitle = json_decode("{$title}", true);
-            
-            //題目
-            foreach($ques_cate_m as $key=>$item){
-                // Log::info($item['category']);
-                $content_ok[$key]['category'] = $item['category'];
-                $content_ok[$key]['ansNO'] = $newTitle[$key];
-                $content_ok[$key]['customize'] = $item['customize'];
-            }
-            
-            foreach($content_ok as $key => $item){
-                //取得多選選項數量
-                $choiceCount = count(json_decode($ques_cate_m[$key]['options'],JSON_UNESCAPED_UNICODE));
-                // Log::info($choiceCount);
-                $tmpChoice = '';
-                for($i=0;$i<=$choiceCount;$i++){
-                    if(in_array($i+1, $item['ansNO'])){
-                        if($item['customize'] == 'Y'){
-                            $tmpChoice .= json_decode($ques_cate_m[$key]['options'],JSON_UNESCAPED_UNICODE)[$i].$item['ansNO'][1];
-                        }else{
-                            $tmpChoice .= json_decode($ques_cate_m[$key]['options'],JSON_UNESCAPED_UNICODE)[$i];
+                //要傳到PDF的因此轉json
+                $content_json_s = json_encode($content_ok_s);
+                // Log::info($content_ok_s);
+                $dom = "<h3>單選</h3>";
+                $dom .= "<a class='btn btn-primary' href='/getDate?val={$newclass01Data[0]['ques_s']}' target='_blank'>下載</a>";
+                $dom .= "<a class='btn btn-danger' href='/pdf?type=s&val={$content_json_s}&val2={$newclass01Data[0]['company']}' target='_blank'>PDF下載</a>";
+                $dom .= "<ul>";
+                foreach($content_ok_s as $item){
+                    $dom .= "<li>[ {$item['ansNO']}.{$item['ansTxt']} ] - {$item['content']}</li>";
+                }
+                $dom .= "</ul>";
+
+
+                // 多選
+                $content_ok_m = [];
+                $question_categories_m = DB::table('question_categories')->select('options', 'category','customize')->where('choice','M')->get();
+                $ques_cate_m = json_decode($question_categories_m, JSON_UNESCAPED_UNICODE);
+                
+                // Log::info($ques_cate_m);
+                //答案
+                // Log::info('112233');
+                // Log::info(json_decode($newclass01Data[0]['ques_m']));
+                //題目
+                foreach($ques_cate_m as $key=>$item){
+                    // Log::info($item['category']);
+                    $content_ok_m[$key]['category'] = $item['category'];
+                    // $content_ok_m[$key]['ansNO'] = json_decode($newclass01Data[0]['ques_m'])[$key];
+                    $content_ok_m[$key]['ansNO'] = json_decode($newclass01Data[0]['ques_m'])[$key];
+                    $content_ok_m[$key]['customize'] = $item['customize'];
+                }
+                // Log::info($content_ok_m);
+                foreach($content_ok_m as $key => $item){
+                    //取得多選選項數量
+                    $choiceCount = count(json_decode($ques_cate_m[$key]['options'],JSON_UNESCAPED_UNICODE));
+                    // Log::info($choiceCount);
+                    $tmpChoice = '';
+                    for($i=0;$i<=$choiceCount;$i++){
+                        if(in_array($i+1, $item['ansNO'])){
+                            if($item['customize'] == 'Y'){
+                                $tmpChoice .= json_decode($ques_cate_m[$key]['options'],JSON_UNESCAPED_UNICODE)[$i].$item['ansNO'][1];
+                            }else{
+                                $tmpChoice .= json_decode($ques_cate_m[$key]['options'],JSON_UNESCAPED_UNICODE)[$i];
+                            }
                         }
                     }
+                    $content_ok_m[$key]['ansTxt'] = $tmpChoice;
                 }
-                $content_ok[$key]['ansTxt'] = $tmpChoice;
+                //要傳到PDF的因此轉json
+                $content_json_m = json_encode($content_ok_m);
+                // Log::info($content_ok_m);
+                $dom .= "<h3>多選</h3>";
+                $dom .= "<a class='btn btn-primary' href='/getDate_multi?val={$newclass01Data[0]['ques_m']}' target='_blank'>下載</a>";
+                $dom .= "<a class='btn btn-danger' href='/pdf?type=m&val={$content_json_m}&val2={$newclass01Data[0]['company']}' target='_blank'>PDF下載</a>";
+                $dom .= "<ul>";
+                foreach($content_ok_m as $item){
+                    $dom .= "<li>[ {$item['ansTxt']} ] - {$item['category']}</li>";
+                }
+                $dom .= "<ul>";
+                return "{$dom}";
             }
-            // Log::info($content_ok);
-            $dom = "<a class='btn btn-primary' href='/getDate_multi?val=$title' target='_blank'>下載</a>";
-            $dom .= "<ul>";
-            foreach($content_ok as $item){
-                $dom .= "<li>[ {$item['ansTxt']} ] - {$item['category']}</li>";
-            }
-            $dom .= "<ul>";
-            return "{$dom}";
         });
+        // $show->field('ques_s', __('Ques_s'))->unescape()->as(function ($title) {
+        //     $content_ok = [];
+        //     //題目
+        //     $questions = DB::table('questions')->select('category_id', 'content')->get();
+        //     $ques = json_decode($questions, JSON_UNESCAPED_UNICODE);
+        //     foreach($ques as $key=>$item){
+        //         array_push($content_ok, $item);
+        //     }
+        //     //答案
+        //     $newTitle = json_decode("{$title}", true);
+        //     $tmpKey = 0;
+        //     foreach($newTitle as $key=>$ansNO){
+        //         $content_ok[$tmpKey]['ansNO'] = $ansNO;
+        //         // array_push($content_ok[$tmpKey], $ansNO);
+        //         $tmpKey++;
+        //     }
+        //     //選項
+        //     $question_categories_s = DB::table('question_categories')->select('options', 'category')->where('choice','S')->get();
+        //     $ques_cate_s = json_decode($question_categories_s, JSON_UNESCAPED_UNICODE);
+        //     $quesCount = DB::table('questions')->select(DB::raw('count(*) as total'))->groupBy('category_id')->get();
+        //     for($i=1;$i<=count($quesCount);$i++){
+        //         foreach($content_ok as $key=>$item){
+        //             if($item['category_id'] == $i){
+        //                 if($item['ansNO'] == 1){
+        //                     $content_ok[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[0];
+        //                 }
+        //                 if($item['ansNO'] == 2){
+        //                     $content_ok[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[1];
+        //                 }
+        //                 if($item['ansNO'] == 3){
+        //                     $content_ok[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[2];
+        //                 }
+        //                 if($item['ansNO'] == 4){
+        //                     $content_ok[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[3];
+        //                 }
+        //                 if($item['ansNO'] == 5){
+        //                     $content_ok[$key]['ansTxt'] = json_decode($ques_cate_s[$i-1]['options'])[4];
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     // Log::info($content_ok);
+        //     $dom = "<a class='btn btn-primary' href='/getDate?val=$title' target='_blank'>下載</a>";
+        //     $dom .= "<ul>";
+        //     foreach($content_ok as $item){
+        //         $dom .= "<li>[ {$item['ansNO']}.{$item['ansTxt']} ] - {$item['content']}</li>";
+        //     }
+        //     $dom .= "<ul>";
+        //     return "{$dom}";
+        // });
+        // $show->field('ques_m', __('Ques_m'))->unescape()->as(function ($title) {
+        //     $content_ok = [];
+        //     $question_categories_m = DB::table('question_categories')->select('options', 'category','customize')->where('choice','M')->get();
+        //     $ques_cate_m = json_decode($question_categories_m, JSON_UNESCAPED_UNICODE);
+            
+        //     // Log::info($ques_cate_m);
+        //     //答案
+        //     $newTitle = json_decode("{$title}", true);
+            
+        //     //題目
+        //     foreach($ques_cate_m as $key=>$item){
+        //         // Log::info($item['category']);
+        //         $content_ok[$key]['category'] = $item['category'];
+        //         $content_ok[$key]['ansNO'] = $newTitle[$key];
+        //         $content_ok[$key]['customize'] = $item['customize'];
+        //     }
+            
+        //     foreach($content_ok as $key => $item){
+        //         //取得多選選項數量
+        //         $choiceCount = count(json_decode($ques_cate_m[$key]['options'],JSON_UNESCAPED_UNICODE));
+        //         // Log::info($choiceCount);
+        //         $tmpChoice = '';
+        //         for($i=0;$i<=$choiceCount;$i++){
+        //             if(in_array($i+1, $item['ansNO'])){
+        //                 if($item['customize'] == 'Y'){
+        //                     $tmpChoice .= json_decode($ques_cate_m[$key]['options'],JSON_UNESCAPED_UNICODE)[$i].$item['ansNO'][1];
+        //                 }else{
+        //                     $tmpChoice .= json_decode($ques_cate_m[$key]['options'],JSON_UNESCAPED_UNICODE)[$i];
+        //                 }
+        //             }
+        //         }
+        //         $content_ok[$key]['ansTxt'] = $tmpChoice;
+        //     }
+        //     // Log::info($content_ok);
+        //     $dom = "<a class='btn btn-primary' href='/getDate_multi?val=$title' target='_blank'>下載</a>";
+        //     $dom .= "<ul>";
+        //     foreach($content_ok as $item){
+        //         $dom .= "<li>[ {$item['ansTxt']} ] - {$item['category']}</li>";
+        //     }
+        //     $dom .= "<ul>";
+        //     return "{$dom}";
+        // });
         $show->field('tax_id_no',__('Chart'))->unescape()->as(function ($title) {
             // return "<a href='/result?class=01&id={$title}' target=_blank>開啟</a>";
             return "<iframe width=100% height=600 src='/result?class=02&id={$title}'>你的瀏覽器不支援 iframe</iframe>";
